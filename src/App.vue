6ebd23afdaac951d4e8c3d770711c3aa65933693
orch20 <94862153+orch20@users.noreply.github.com>
2023-10-18 10:55:07 +0300

    initial files

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <NavBar />
+    <div class="container">
+      <RouterView />
+    </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import NavBar from './components/Layout/NavBar.vue';
+
+</script>
+
+
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/Layout/NavBar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>hello</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
