e8a436c37b90449c442f9e91659c6793494c0b07
orch20 <94862153+orch20@users.noreply.github.com>
2023-10-19 16:24:24 +0300

    added section What makes us different,  markup and scss without slider

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,14 @@
 <template>
     <main>
         <SearchSection />
+        <SecondIndexPageSection />
     </main>
     
 </template>
 
 <script setup>
 import SearchSection from '../components/Sections/SearchSection.vue'
+import SecondIndexPageSection from '../components/Sections/SecondIndexPageSection.vue';
 
 </script>
 
