e88bb8b866e7bcf486884c1605380c4c834dcd5e
orch20 <94862153+orch20@users.noreply.github.com>
2023-10-19 19:42:53 +0300

    added TextAnimation section & customed animation

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,6 +2,7 @@
     <main>
         <SearchSection />
         <SecondIndexPageSection />
+        <TextAnimationSection />
         <Footer/>
     </main>
     
@@ -11,6 +12,7 @@
 import SearchSection from '../components/Sections/SearchSection.vue'
 import SecondIndexPageSection from '../components/Sections/SecondIndexPageSection.vue';
 import Footer from '../components/Sections/Footer.vue';
+import TextAnimationSection from '../components/Sections/TextAnimationSection.vue';
 
 </script>
 
