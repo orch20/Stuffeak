5cdcb082fb165ef77093e94dcf286af91e9a8a7a
orch20 <94862153+orch20@users.noreply.github.com>
2023-10-20 15:50:44 +0300

    added  temporary infitite scrollig in the SearchSection

--- src/components/Elements/LogoSlider.vue
+++ src/components/Elements/LogoSlider.vue
@@ -12,6 +12,17 @@
       <div class="logo"><img src="../../img/logos/logo-07.svg" alt=""> </div>
       <div class="logo"><img src="../../img/logos/logo-08.svg" alt=""> </div>
       <div class="logo"><img src="../../img/logos/logo-09.svg" alt=""> </div>
+      <div class="logo"><img src="../../img/logos/logo-01.svg" alt=""> </div>
+      <div class="logo"><img src="../../img/logos/logo-02.svg" alt=""> </div>
+      <div class="logo"><img src="../../img/logos/logo-03.svg" alt=""> </div>
+      <div class="logo"><img src="../../img/logos/logo-04.svg" alt=""> </div>
+      <div class="logo"><img src="../../img/logos/logo-05.svg" alt=""> </div>
+      <div class="logo"><img src="../../img/logos/logo-06.svg" alt=""> </div>
+      <div class="logo"><img src="../../img/logos/logo-07.svg" alt=""> </div>
+      <div class="logo"><img src="../../img/logos/logo-08.svg" alt=""> </div>
+      <div class="logo"><img src="../../img/logos/logo-09.svg" alt=""> </div>
+      
+      
     </div>
     <!--END   logos-slider-->
   </div>
@@ -19,6 +30,7 @@
 
 <style lang="scss" scoped>
 @import '../../assets/main.scss';
+
 
 .logos-slider-wrap {
   position: absolute;
@@ -39,6 +51,10 @@
   text-align: center;
   color: $light-grey;
 }
+@keyframes infiniteScroll {
+  from {transform: translateX(0) }
+  to {transform: translateX(calc(0px - 50% - 25px));}
+}
 
 .logos-wrap {
   margin: 0 auto;
@@ -46,9 +62,14 @@
   display: flex;
   align-items: center;
   justify-content: center;
+  animation-name: infiniteScroll;
+  animation-duration: 35s;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
 }
 
 .logo {
   margin-right: 60px;
+  white-space: nowrap;
 }
 </style>
