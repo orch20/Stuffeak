62fbe147a0fcd2f823923bc8c67d486245fb3674
orch20 <94862153+orch20@users.noreply.github.com>
2023-10-20 14:01:40 +0300

    refactored WhatMakeUsDif Component

---
+++ src/components/Elements/WhatMakesUsDifSlider.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="second-slider swiper-initialized swiper-horizontal swiper-pointer-events swiper-backface-hidden" id="second-slider">
+        <div class="swiper-wrapper" id="swiper-wrapper-3d802f4d1c56161a" aria-live="polite" style="transform: translate3d(0px, 0px, 0px); transition-duration: 0ms;">
+        <div class="swiper-slide second-slider__slide swiper-slide-active" role="group" aria-label="1 / 3" style="width: 291px; margin-right: 30px;">
+            <div class="second-slider__icon"><img src="../../img/second-slider-icon-01.svg" alt="icon"></div>
+            <div class="second-slider__text">We provide qualified and professional personnel - from office workers to medicine personal. All types of staff what you need</div>
+        </div>
+        <div class="swiper-slide second-slider__slide swiper-slide-next" role="group" aria-label="2 / 3" style="width: 291px; margin-right: 30px;">
+            <div class="second-slider__icon"><img src="../../img/second-slider-icon-02.svg" alt="icon"></div>
+            <div class="second-slider__text">Actively recruit thousand’s of talented professionals that are unable to see your job posts</div>
+        </div>
+        <div class="swiper-slide second-slider__slide" role="group" aria-label="3 / 3" style="width: 291px; margin-right: 30px;">
+            <div class="second-slider__icon"><img src="../../img/second-slider-icon-03.svg" alt="icon"></div>
+            <div class="second-slider__text">We screen candidates and handle the time-consuming work, so that you can focus on hiring those who suit your needs best</div>
+        </div>
+        </div>
+        <div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets swiper-pagination-horizontal"><span class="swiper-pagination-bullet swiper-pagination-bullet-active" tabindex="0" role="button" aria-label="Go to slide 1" aria-current="true"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 2"></span></div>
+    <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
+</template>
+
+<style>
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,7 @@
 <template>
     <main>
         <SearchSection />
-        <SecondIndexPageSection />
+        <WhatMakesUsDifSection />
         <WhyWeSection />
         <TextAnimationSection />
         <Footer/>
@@ -11,7 +11,7 @@
 
 <script setup>
 import SearchSection from '../components/Sections/SearchSection.vue'
-import SecondIndexPageSection from '../components/Sections/SecondIndexPageSection.vue';
+import WhatMakesUsDifSection from '../components/Sections/WhatMakesUsDifSection.vue';
 import WhyWeSection from '../components/Sections/WhyWeSection.vue';
 import TextAnimationSection from '../components/Sections/TextAnimationSection.vue';
 import Footer from '../components/Sections/Footer.vue';
